4ead5f8378868d1e45422ddf8fa83cf880eee7a2
PROG17tomas <Tomas.MohallNilson@yh.nackademin.se>
2017-09-19 11:22:48 +0200

    commit

---
+++ DungeonCrawler/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonCrawler
+{
+    class DungeonCrawler
+    {
+        static void Main(string[] args)
+        {
+            const int windowWidth = 120;
+            const int windowHeight = 30;
+
+            Console.SetWindowSize(windowWidth, windowHeight);
+
+            Menu.Run();
+        }
+    }
+}
