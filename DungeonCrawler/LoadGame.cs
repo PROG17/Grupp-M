6495bc735b70b5c71ee59b12c47bef4bb0f0e35f
PROG17tomas <Tomas.MohallNilson@yh.nackademin.se>
2017-09-20 19:12:27 +0200

    add comment

--- DungeonCrawler/LoadGame.cs
+++ DungeonCrawler/LoadGame.cs
@@ -22,6 +22,8 @@ namespace DungeonCrawler
 
         private static void LoadRooms()
         {
+            // Load all rooms
+
             Globals.rooms.Add("entrance", new Room("Entrance","You're standing in a large open hallway. In front of you there is an old stairway leading up to the second floor of the mansion. To the left there is a sturdy door behind a bookshelf. There is a large [chandelier] covored in cobweb hanging from the ceiling."));
             Globals.rooms.Add("bathroom", new Room("Luxurious bathroom", "A white [throne] decorates this room.","When looking carefully, you find a [note] behind the toilet."));
         }
